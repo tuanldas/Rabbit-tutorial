f21f37fcde2372658e5ee498fdf40004119545ea
tuanldas <tuanldas@gmail.com>
2022-10-31 14:44:17 +0700

    Commment code
    Signed-off-by: tuanldas <tuanldas@gmail.com>

--- src/1-hello-world/receiving.php
+++ src/1-hello-world/receiving.php
@@ -3,8 +3,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
+// Tạo một kết nối tời máy chủ
 $connection = new AMQPStreamConnection('rabbit', 5672, 'tuanld', '123123');
 $channel = $connection->channel();
+
+// Khai báo hàng đợi sẽ tiêu thụ
 $channel->queue_declare('hello', false, false, false, false);
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
 $callback = function ($msg) {
--- src/1-hello-world/send.php
+++ src/1-hello-world/send.php
@@ -4,11 +4,18 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+// Tạo một kết nối tời máy chủ
 $connection = new AMQPStreamConnection('rabbit', 5672, 'tuanld', '123123');
 $channel = $connection->channel();
+
+// Tạo một hàng đợi để gửi đến. Sau đó xuất bản một tin nhắn
 $channel->queue_declare('hello', false, false, false, false);
 $msg = new AMQPMessage('Hello World!');
 $channel->basic_publish($msg, '', 'hello');
 echo " [x] Sent 'Hello World!'\n";
+
+// Một hàng đợi là idempotent - nó chỉ được tạo nếu không tồn tại. Nội dung tin nhắn là mảng byte, vì vậy có thể mã hóa bất kỳ thừ gì ở đó.
+
+// Đóng kết nối
 $channel->close();
 $connection->close();
