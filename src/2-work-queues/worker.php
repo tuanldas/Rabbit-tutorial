5766ccf765b9d6cba9525946e44d54c6eed3e973
tuanldas <tuanldas@gmail.com>
2022-11-01 11:17:28 +0700

    2. Work Queues
    Signed-off-by: tuanldas <tuanldas@gmail.com>

---
+++ src/2-work-queues/worker.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection('rabbit', 5672, 'tuanld', '123123');
+$channel = $connection->channel();
+
+$channel->queue_declare('task_queue', false, true, false, false);
+
+echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+$callback = function ($msg) {
+echo ' [x] Received ', $msg->body, "\n";
+sleep(substr_count($msg->body, '.'));
+echo " [x] Done\n";
+$msg->ack();
+};
+
+$channel->basic_qos(0, 1, false);
+$channel->basic_consume('task_queue', '', false, false, false, false, $callback);
+
+while ($channel->is_open()) {
+$channel->wait();
+}
+
+$channel->close();
+$connection->close();
